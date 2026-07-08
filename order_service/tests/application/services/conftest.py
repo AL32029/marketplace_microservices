3959a24c577ffa850e5f6f4188fec4e4190c2461
Nikita Bondarev <nikitabondarevvitebsk@gmail.com>
2026-07-08 21:23:24 +0300

    Микросервис "Order Service" - Добавлен файл conftest.py для общих фикстур - Уточнена структура файлов с тестами

---
+++ order_service/tests/application/services/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from order_service.domain.entities.order import Order, OrderItem
+from order_service.infrastructure.db.models import Base
+from order_service.infrastructure.repositories.sqlalchemy_order_repo import SQLALchemyOrderRepo
+
+
+@pytest.fixture
+async def async_session_fixture(tmp_path):
+    db_file = tmp_path / "test.db"
+    engine = create_async_engine(
+        f"sqlite+aiosqlite:///{db_file}",
+        poolclass=NullPool,
+    )
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
+
+    await engine.dispose()
+
+
+@pytest.fixture
+async def order_repo(async_session_fixture):
+    return SQLALchemyOrderRepo(async_session_fixture)
+
+
+@pytest.fixture
+def order_item():
+    return Order(user_id=1, items=[OrderItem(product_id=1, quantity=2, price=10.0)])
--- order_service/tests/domain/entities/test_order.py
+++ order_service/tests/domain/entities/test_order.py
@@ -3,29 +3,27 @@ import pytest
 from order_service.domain.entities.order import Order, OrderStatus
 
 
-@pytest.fixture
-def order_item():
-    return Order(user_id=1)
-
-
-def test_add_item_recalculates_total(order_item):
+def test_add_item_recalculates_total():
     """Тестирование добавления предмета и пересчета стоимости"""
-    result = order_item.add_item(product_id=1, quantity=1, price=5)
+    order = Order(user_id=1)
+    result = order.add_item(product_id=1, quantity=1, price=5)
 
-    assert result in order_item.items
-    assert order_item.total == (result.price * result.quantity)
+    assert result in order.items
+    assert order.total == (result.price * result.quantity)
 
 
-def test_cancel_changes_status_to_cancelled(order_item):
+def test_cancel_changes_status_to_cancelled():
     """Тестирование отмены заказа"""
-    order_item.cancel()
+    order = Order(user_id=1)
+    order.cancel()
 
-    assert order_item.status == OrderStatus.CANCELLED
+    assert order.status == OrderStatus.CANCELLED
 
 
-def test_cancel_raises_error_if_paid(order_item):
+def test_cancel_raises_error_if_paid():
     """Тестирование ошибки отмены заказа при статусе Оплачен"""
-    order_item.status = OrderStatus.PAID
+    order = Order(user_id=1)
+    order.status = OrderStatus.PAID
 
     with pytest.raises(ValueError):
-        order_item.cancel()
+        order.cancel()
