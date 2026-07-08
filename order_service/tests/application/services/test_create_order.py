89b63afc345934cf102611a17be1aa6105f0dd47
Nikita Bondarev <nikitabondarevvitebsk@gmail.com>
2026-07-08 21:23:36 +0300

    Микросервис "Order Service" - Добавлен файл conftest.py для общих фикстур - Уточнена структура файлов с тестами

--- order_service/tests/application/services/test_create_order.py
+++ order_service/tests/application/services/test_create_order.py
@@ -1,41 +1,9 @@
 from unittest.mock import AsyncMock
 
 import pytest
-from sqlalchemy import NullPool
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from order_service.application.services.create_order import CreateOrderUseCase
-from order_service.domain.entities.order import Order, OrderItem
-from order_service.infrastructure.db.models import Base
-from order_service.infrastructure.repositories.sqlalchemy_order_repo import SQLALchemyOrderRepo
-
-
-@pytest.fixture
-async def async_session_fixture(tmp_path):
-    db_file = tmp_path / "test.db"
-    engine = create_async_engine(
-        f"sqlite+aiosqlite:///{db_file}",
-        poolclass=NullPool,
-    )
-
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
-    async with async_session() as session:
-        yield session
-
-    await engine.dispose()
-
-
-@pytest.fixture
-async def order_repo(async_session_fixture):
-    return SQLALchemyOrderRepo(async_session_fixture)
-
-
-@pytest.fixture
-def order_item():
-    return Order(user_id=1, items=[OrderItem(product_id=1, quantity=2, price=10.0)])
+from order_service.tests.application.services.conftest import order_repo, order_item
 
 
 @pytest.mark.asyncio
