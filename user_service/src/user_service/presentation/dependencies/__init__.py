757fa69cebeb552187c77d17910f721d4643dc8c
Nikita Bondarev <nikitabondarevvitebsk@gmail.com>
2026-07-11 22:25:48 +0300

    Микросервис "User Service" - Внесены мелкие изменения

--- user_service/src/user_service/presentation/dependencies/__init__.py
+++ user_service/src/user_service/presentation/dependencies/__init__.py
@@ -6,6 +6,6 @@ __all__ = [
 from .db.get_db_session import get_db
 from .repositories.jwt_token_generator import jwt_token_generator_repo_depends
 from .repositories.password_hasher import password_hasher_repo_depends
+from .use_cases.current_profile import get_current_user
 from .use_cases.login_user import login_user_use_case_depends
 from .use_cases.register_user import register_user_use_case_depends
-from .use_cases.current_profile import get_current_user
--- user_service/src/user_service/presentation/dependencies/use_cases/login_user.py
+++ user_service/src/user_service/presentation/dependencies/use_cases/login_user.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from user_service.application.ports.password_hasher_repo import PasswordHasherRepo
 from user_service.application.ports.token_generator import TokenGeneratorRepo
 from user_service.application.services import LoginUserUseCase
-from user_service.infrastructure.repositories import JWTTokenGeneratorRepo
 from user_service.presentation.dependencies.db.get_db_session import get_db
 from user_service.presentation.dependencies.repositories.jwt_token_generator import jwt_token_generator_repo_depends
 from user_service.presentation.dependencies.repositories.password_hasher import password_hasher_repo_depends
